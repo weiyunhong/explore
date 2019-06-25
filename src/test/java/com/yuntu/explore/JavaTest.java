9df33533c6635c89afbee058a38b999c0c585f46
weiyh <weiyh@yimidda.com>
2019-06-25 17:17:02 +0800

    测试类提交

---
+++ src/test/java/com/yuntu/explore/JavaTest.java
@@ -0,0 +1,26 @@
+package com.yuntu.explore;
+
+import java.util.*;
+
+public class JavaTest {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+    private void CollectionTest(){
+        // 集合类
+        Map map = new HashMap();
+
+        Set set = new HashSet();
+
+        List list = new ArrayList();
+
+        list.add("a");
+        list.add(1);
+
+    }
+
+
+
+}
